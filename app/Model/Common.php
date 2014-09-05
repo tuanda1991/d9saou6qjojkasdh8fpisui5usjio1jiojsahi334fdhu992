a52878e3002f31e8de55e425d4653b516b7dee04
anhtuan <anhtuan@anhtuan-HP-Pro-3330-MT>
2014-09-05 11:35:59 +0700

    test git

--- app/Model/Common.php
+++ app/Model/Common.php
@@ -1,52 +1,65 @@
 <?php
+App::uses('AppModel', 'Model');
 /**
- * Common Model.
+ * Common Model is a model content all common values of project
  * 
- * @copyright     Copyright (c) Fingerprint Team Foundation, Inc. (http://cakefoundation.org)
- * @link          http://fingerprint.com.vn/basic_cakephp BasicCakePHP Project
+ * @author doanhtuan1991@gmail.com
+ * @copyright     never
+ * @link          http://fingerprint.com.vn/
  * @package       app.Model
+ * @version       1.0
  * @since         BasicCakePHP v 0.2.9
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
 class Common extends AppModel{
-    public $useTable=false;
+    
+    public $useTable=false;    
     
     public function __construct($id = false, $table = null, $ds = null) {
         parent::__construct($id, $table, $ds);
+    }  
+    
+    /**
+     * this function used to get current url
+     * 
+     * @author doanhtuan1991@gmail.com
+     * @param type $onlyRoot
+     * @return string
+     */
+    public function getCurrentURL($onlyRoot='')
+    {
+        $currentURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
+        $currentURL .= $_SERVER["SERVER_NAME"];
+
+        if($_SERVER["SERVER_PORT"] != "80" && $_SERVER["SERVER_PORT"] != "443")
+        {
+            $currentURL .= ":".$_SERVER["SERVER_PORT"];
+        }
+        if($onlyRoot==''){
+        $currentURL .= $_SERVER["REQUEST_URI"];
+        }
+        return $currentURL;
     }
     
     /**
-     * this function used to get common values of page
+     * shortcutString is a function used to get shortcut
      * 
-     * @author tuanda
-     * @param String $pageName this is pagename
-     * @return array this is all values of page
-     */
-    
-    public function getPageVals($pageName){
-        $array=array();
-        return $array;
+     * @author doanhtuan1991@gmail.com
+     * @param string $str
+     * @param int $max this is max length of Return-String
+     * @param bit $isHover if you want show tooltip when user hover it, you need set 1 else 0
+     * @return string
+     */    
+    public function shortcutString($str="",$max=15,$isHover=1){
+        $item=$str;        
+        if(strlen($item)<$max) return $item;                                                               
+        if($isHover!=1)
+        {
+            return substr(htmlentities($item), 0,$max)." (...)";
+        }
+        return '<a title="'.htmlentities($item).'" >'.substr(htmlentities($item), 0,$max)." (...)</a>";
     }
-    
-/*
- * @param Type [$varname] this is a param of function
- * @return Type return type of function
- * @var Type Documents the data type for a class variable or constant
- * @author Author name <author@email> Documents the author of the current element
- * @copyright Name date Documents copyright information
- * @license URL name 
- * @version Version string Provides the version number of a class or method
- * @since Documents the release version
- * @deprecated Is used to indicate which elements are deprecated and are to be removed in a future version
- * @todo Information string Documents things that need to be done to the code at a later date
- * @example /path/to/example 
- * @link URL link text 
- * @see Documents any element
- * @uses Documents how the element is used
- * @package Name of a package Documents a group of related classes and functions
- * @subpackage 
-*/
     
 }
 
