7722de7c846041fa106c8da002805d6941378150
anhtuan <anhtuan@anhtuan-HP-Pro-3330-MT>
2014-09-05 15:00:28 +0700

    test git

---
+++ app/Model/PageRole.php
@@ -0,0 +1,23 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Description PageRole Model is a model content all common values of project
+ * 
+ * @author doanhtuan1991@gmail.com
+ * @copyright     never
+ * @link          http://fingerprint.com.vn/
+ * @package       app.Model
+ * @version       1.0
+ * @since         BasicCakePHP v 0.2.9
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+class PageRole extends AppModel{
+    public $values= array(
+        '1'=> array(
+            'Name'=>'user_list',            
+        ),
+        '2'=> array(
+            'Name'=>'user_group_list',            
+        ),
+    );    
+}
